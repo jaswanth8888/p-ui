a386394d375f9092209be18103f586efc7784ce4
KARANI JASWANTH <karani.jaswanth@publicissapient.com>
2020-03-19 15:01:07 +0530

    changed uri

--- src/redux/actions/RetailerActions.jsx
+++ src/redux/actions/RetailerActions.jsx
@@ -28,7 +28,7 @@ export const postZone = (zoneDetails) =>async (dispatch) => {
 }
 
 export const postCluster = (cluster,zone) => async(dispatch)=>{
-    await axios.put(RETAILER_BASE_URL+'/location-management/'+zone+'/addcluster',cluster,{ headers: {"Authorization" : TOKEN} }).then((res)=>{
+    await axios.put(RETAILER_BASE_URL+'/location-management/'+zone+'/cluster',cluster,{ headers: {"Authorization" : TOKEN} }).then((res)=>{
         dispatch({type:CREATE_CLUSTER,msg:"Cluster for Zone "+ zone+ " is Created Successfully"})
     }).catch((err)=>{
         dispatch({type:CREATE_CLUSTER, msg:"Sorry Cluster already exists"})
@@ -45,7 +45,7 @@ export const getZones = () => async (dispatch) => {
 }
 
 export const getClusters= (zone) => async(dispatch) => {
-    await axios.get(RETAILER_BASE_URL + '/location-management/'+zone+'/cluster',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
+    await axios.get(RETAILER_BASE_URL + '/location-management/'+zone+'/clusters',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
         dispatch({type:CLUSTER_GET_REQUEST, clusters:res.data})
     }).catch((err)=>{
         dispatch({type:FAILURE})
@@ -53,7 +53,7 @@ export const getClusters= (zone) => async(dispatch) => {
 }
 
 export const postStore = (store,zone,cluster) => async(dispatch) => {
-    await axios.put(RETAILER_BASE_URL+'/location-management/'+zone+'/'+cluster+'/addstore',store,{ headers: {"Authorization" : TOKEN} }).then((res)=>{
+    await axios.put(RETAILER_BASE_URL+'/location-management/'+zone+'/'+cluster+'/store',store,{ headers: {"Authorization" : TOKEN} }).then((res)=>{
         dispatch({type:STORE_POST_REQUEST, msg:"Store Created Succesfully"})
     }).catch((err)=>{
         dispatch({type:FAILURE})
@@ -68,14 +68,14 @@ export const messageSetNull=() =>(dispatch) =>{
     dispatch({type:MESSAGE_SET_NULL})
 }
 export const getZoneList = () => async (dispatch) => {
-    await axios.get(RETAILER_BASE_URL + '/location-management/zonelist',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
+    await axios.get(RETAILER_BASE_URL + '/location-management/zone-map',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
         
         dispatch({type:ZONELIST_GET_REQUEST, zoneList:res.data})
     });
 }
 
 export const getClusterList = () => async (dispatch) => {
-    await axios.get(RETAILER_BASE_URL + '/location-management/clusterlist',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
+    await axios.get(RETAILER_BASE_URL + '/location-management/cluster',{ headers: {"Authorization" : TOKEN} }).then((res)=>{
         
         dispatch({type:CLUSTERLIST_GET_REQUEST, clusterList:res.data})
     });
