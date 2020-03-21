081d2ca823c4fa41f348aa7874e8cbb982b72fbe
KARANI JASWANTH <karani.jaswanth@publicissapient.com>
2020-03-21 12:06:21 +0530

    added Component

---
+++ src/components/retailer/AddGroup.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from 'react'
+import { Grid, TextField, Avatar, Typography, Box ,Icon} from "@material-ui/core";
+import Button from "../atoms/Button";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import { connect } from "react-redux";
+
+
+
+export default class AddGroup extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             group:{
+                 groupName:''
+             },
+             error:{
+                 groupNameError:false,
+                 groupNameErrorMsg:''
+             }
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        let group = this.state.group;
+        console.log(group,name,value)
+        group[name] = value;
+        this.setState({ group });
+      }
+
+
+    is_validGroupName=()=>{
+        let groupName=this.state.group.groupName
+        let error=this.state.error
+        if(groupName===''){
+          error.groupNameError=true;
+          error.groupNameErrorMsg='please fill Group Name'
+          this.setState({error})
+          return false
+        }
+        error.groupNameError=false;
+        error.groupNameErrorMsg=''
+        this.setState({error})
+    
+        return true
+    
+      }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.is_validGroupName()) {
+        //   this.props.login({ ...this.state.user_crendentials }); // thunk action
+        }
+      }
+    
+    render() {
+        return (
+            <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "100vh" }}
+          >
+            <Grid item xs={3}>
+              <Box diasplay="flex" flexDirection="row" justifyContent="center">
+                <Box p={1}>
+                  <Avatar
+                    className="{classes.avatar}"
+                    style={{ color: "#3F51B5" }}
+                  >
+                    <AddCircleOutlineIcon fontSize="large"/>
+                  </Avatar>
+                </Box>
+
+                <Typography component="h1" variant="h5">
+                  Add group
+                </Typography>
+                <Typography component="span" color="error" variant="h5">
+                  {/* {this.props.login_status.errorMsg} */}
+                </Typography>
+              </Box>
+              <form className="{classes.form}" noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="groupName"
+                  label="Group Name"
+                  name="groupName"
+                  error={this.state.error.groupNameError}
+                  helperText={this.state.error.groupNameErrorMsg}
+                  onChange={this.handleChange}
+                  autoFocus
+                />
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className="{classes.submit}"
+                  onClick={this.handleSubmit}
+                >
+                  Save
+                </Button>
+              </form>
+            </Grid>
+          </Grid>
+            )
+    }
+}
