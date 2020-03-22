b0390f8f12935f7c5c011f579b158375216e013f
mayrao1 <mayrao1@publicisgroupe.net>
2020-03-22 21:01:42 +0530

    Table changes

--- src/components/retailer/ViewClusters.jsx
+++ src/components/retailer/ViewClusters.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import {getClusterList} from '../../redux/actions/RetailerActions'
-import { Table } from '@material-ui/core';
+import { getClusterList } from '../../redux/actions/RetailerActions'
+import { Avatar, Box, Grid, Table } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
@@ -11,46 +11,61 @@ import Paper from '@material-ui/core/Paper';
 class ViewClusters extends Component {
     constructor(props) {
         super(props)
-    
+
         this.props.getClusterList();
     }
 
-    
-    render() {
-    
-        return (
-            
-          <div className="container">
-              <br/><br/><br/><br/>
-          <TableContainer component={Paper}>
-              <Table size="small" aria-label="a dense table">
 
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>Cluster Name</TableCell>
-                          <TableCell>Number of Stores</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <tbody>
-                      {Object.keys(this.props.clusterList).map((i)=>{
-                          return <TableRow>
-                            <TableCell key={i} value={i}>{i}</TableCell>
-                            <TableCell>{this.props.clusterList[i]}</TableCell>
-                          </TableRow>
-                      })}
-                  </tbody>
-              </Table>
-              </TableContainer>
-          </div> 
+    render() {
+
+        return (
+
+            <div className="container">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minHeight: "110vh", marginTop: "-400px" }}
+
+                >
+
+                    <Grid item xs={3} style={{
+                        borderRadius: "4px",
+                        padding: "40px",
+                        position: "relative"
+                    }}>
+                        <TableContainer component={Paper}>
+                            <Table size="small" aria-label="a dense table">
+                                <TableHead style = {{ backgroundColor : "#C60078"}}>
+                                    <TableRow>
+                                        <TableCell style={{color:"#FFF"}}>Cluster Name</TableCell>
+                                        <TableCell style={{color:"#FFF"}}>Number of Stores</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <tbody>
+                                    {Object.keys(this.props.clusterList).map((i) => {
+                                        return <TableRow>
+                                            <TableCell key={i} value={i}>{i}</TableCell>
+                                            <TableCell>{this.props.clusterList[i]}</TableCell>
+                                        </TableRow>
+                                    })}
+                                </tbody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                </Grid>
+            </div>
         )
-        }
+    }
 }
 
 const stateAsProps = (store) => ({
     clusterList: store.RetailerReducer.clusterList
-  });
-  const actionAsProps = {
+});
+const actionAsProps = {
     getClusterList: getClusterList
-    
-  }
-  export default connect(stateAsProps,actionAsProps)(ViewClusters);
+
+}
+export default connect(stateAsProps, actionAsProps)(ViewClusters);
--- src/components/retailer/ViewZones.jsx
+++ src/components/retailer/ViewZones.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import {getZoneList} from '../../redux/actions/RetailerActions'
-import { Table } from '@material-ui/core';
+import { getZoneList } from '../../redux/actions/RetailerActions'
+import { Avatar, Box, Grid, Table } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
@@ -14,42 +14,58 @@ class ViewZones extends Component {
         this.props.getZoneList();
     }
 
-    
-    render() {
-    
-        return (
-            
-          <div className="container">
-              <br/><br/><br/><br/>
-          <TableContainer component={Paper}>
-              <Table size="small" aria-label="a dense table">
 
-                  <TableHead>
-                      <TableRow>
-                          <TableCell>Zone Name</TableCell>
-                          <TableCell>Number of Clusters</TableCell>
-                      </TableRow>
-                  </TableHead>
-                  <tbody>
-                      {Object.keys(this.props.zoneList).map((i)=>{
-                          return <TableRow>
-                            <TableCell key={i} value={i}>{i}</TableCell>
-                            <TableCell>{this.props.zoneList[i]}</TableCell>
-                          </TableRow>
-                      })}
-                  </tbody>
-              </Table>
-              </TableContainer>
-          </div> 
+    render() {
+
+        return (
+
+            <div className="container">
+                <Grid
+                    container
+                    spacing={0}
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    style={{ minHeight: "110vh", marginTop: "-400px" }}
+
+                >
+
+                    <Grid item xs={3} style={{
+                        borderRadius: "4px",
+                        padding: "40px",
+                        position: "relative"
+                    }}>
+                        <TableContainer component={Paper}>
+                            <Table size="small" aria-label="a dense table">
+
+                                <TableHead style={{ backgroundColor: "#C60078" }}>
+                                    <TableRow>
+                                        <TableCell style={{ color: "#FFF" }}>Zone Name</TableCell>
+                                        <TableCell style={{ color: "#FFF" }}>Number of Clusters</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <tbody>
+                                    {Object.keys(this.props.zoneList).map((i) => {
+                                        return <TableRow>
+                                            <TableCell key={i} value={i}>{i}</TableCell>
+                                            <TableCell>{this.props.zoneList[i]}</TableCell>
+                                        </TableRow>
+                                    })}
+                                </tbody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                </Grid>
+            </div>
         )
-        }
+    }
 }
 
 const stateAsProps = (store) => ({
     zoneList: store.RetailerReducer.zoneList
-  });
-  const actionAsProps = {
+});
+const actionAsProps = {
     getZoneList: getZoneList
-    
-  }
-  export default connect(stateAsProps,actionAsProps)(ViewZones);
+
+}
+export default connect(stateAsProps, actionAsProps)(ViewZones);
