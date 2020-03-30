2db039fe9638761e59db872bf56bc4ed2ce22ea1
B Yatheesha <byathe@publicisgroupe.net>
2020-03-30 17:52:58 +0530

    integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import Navbar from './components/organisms/Navbar';
 import rootReducer from './redux/reducers/rootReducer';
-
+import VendorLogin from './components/vendor/VendorLogin';
+import Registration from './components/vendor/Registration'
+import Home from './components/vendor/Home';
+import Addproduct from './components/vendor/AddProduct';
+import { BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 let state = window.sessionStorage.reduxstate;
 if (state) {
@@ -32,10 +37,49 @@ store.subscribe(() => {
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+        isvendor:false
+    }
+    this.changeTOVendor = this.changeTOVendor.bind(this);
+    this.changeTORetailer = this.changeTORetailer.bind(this);
+}
+changeTOVendor(e) {
+    let isvendor=true
+    this.setState({isvendor})
+}
+changeTORetailer(e){
+    let isvendor=false
+    this.setState({isvendor})
+}
   render() {
     return (
       <Provider store={store}>
+
+        {this.state.isvendor===false &&
+        <div>
+          <Button
+                        onClick={this.changeTOVendor} 
+                        >vendorlogin</Button>
+                        <Button>Vendor Register</Button>
         <Navbar />
+        </div>}
+        {this.state.isvendor===true && 
+        <div>
+          <Button
+                        onClick={this.changeTORetailer}>
+                            Retailer Login</Button>
+        <Router >
+                  <Switch>
+                    <Route exact={true} path="/" component={VendorLogin} />
+                    <Route exact={true} path="/" component={Registration} />
+                    <Route exact path="/home" component={Home}/>               
+                    <Route exact={true} path="/addproduct" component={Addproduct}/>  
+                    </Switch>
+                </Router>
+          </div>
+  }
       </Provider>
     )
   }
--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import Button from '@material-ui/core/Button';
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App1 from "./App1"
-import App2 from "./App2"
+
 import deepPurple from "@material-ui/core/colors/deepPurple";
 import amber from "@material-ui/core/colors/amber";
 import "./i18n";
@@ -28,7 +27,7 @@ let isvendor=false
 ReactDOM.render(
   <Suspense fallback={<div>Loading</div>}>
     <MuiThemeProvider theme={theme}>
-    <App2></App2>
+    <App></App>
 
     </MuiThemeProvider>
   </Suspense>,
