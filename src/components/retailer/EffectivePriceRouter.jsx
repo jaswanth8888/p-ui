6488d93c79014710e9a4a16c91adc4619f8b0c08
KARANI JASWANTH <karani.jaswanth@publicissapient.com>
2020-04-06 16:11:47 +0530

    solved routing issue

--- src/components/retailer/EffectivePriceRouter.jsx
+++ src/components/retailer/EffectivePriceRouter.jsx
@@ -1,31 +1,26 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom"
+import React from "react"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
 import PrivateRoute from "../utils/privateRoute"
 import GetProductsInDateRange from "./GetProductsInDateRange"
 import ViewEffectiveDatesAndPrices from "./ViewEffectiveDatesAndPrices"
 
-export default class EffectivePriceRouter extends Component {
-  componentWillMount() {
-    this.props.history.push("/view/products/daterange")
-  }
-
-  render() {
-    return (
-      <Router>
-        <Redirect to="/view/products/daterange" />
-        <Switch>
-          <PrivateRoute
-            exact
-            path="/view/products/daterange"
-            component={GetProductsInDateRange}
-          />
-          <PrivateRoute
-            exact
-            path="/view/effectiveprices"
-            component={ViewEffectiveDatesAndPrices}
-          />
-        </Switch>
-      </Router>
-    )
-  }
+const EffectivePriceRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute
+          exact
+          path="/view/products/daterange"
+          component={GetProductsInDateRange}
+        />
+        <PrivateRoute
+          exact
+          path="/view/effectiveprices"
+          component={ViewEffectiveDatesAndPrices}
+        />
+      </Switch>
+    </Router>
+  )
 }
+
+export default EffectivePriceRouter
--- src/components/retailer/ZoneClusterRouter.jsx
+++ src/components/retailer/ZoneClusterRouter.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom"
+import React from "react"
+import { BrowserRouter as Router, Switch } from "react-router-dom"
 import PrivateRoute from "../utils/privateRoute"
 import SelectProduct from "./SelectProduct"
 import AssignToZone from "./AssignToZone"
@@ -7,35 +7,29 @@ import AssignToCluster from "./AssignToCluster"
 import ViewAssignedZones from "./ViewAssignedZones"
 import ViewAssignedClusters from "./ViewAssignedClusters"
 
-export default class ZoneClusterRouter extends Component {
-  componentWillMount() {
-    this.props.history.push("/products/assign")
-  }
-
-  render() {
-    return (
-      <Router>
-        <Redirect to="/selectproduct" />
-        <Switch>
-          <PrivateRoute exact path="/selectproduct" component={SelectProduct} />
-          <PrivateRoute
-            exact
-            path="/assigntocluster"
-            component={AssignToCluster}
-          />
-          <PrivateRoute exact path="/assigntozone" component={AssignToZone} />
-          <PrivateRoute
-            exact
-            path="/view/assigned/zones"
-            component={ViewAssignedZones}
-          />
-          <PrivateRoute
-            exact
-            path="/view/assigned/clusters"
-            component={ViewAssignedClusters}
-          />
-        </Switch>
-      </Router>
-    )
-  }
+const ZoneClusterRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <PrivateRoute exact path="/selectproduct" component={SelectProduct} />
+        <PrivateRoute
+          exact
+          path="/assigntocluster"
+          component={AssignToCluster}
+        />
+        <PrivateRoute exact path="/assigntozone" component={AssignToZone} />
+        <PrivateRoute
+          exact
+          path="/view/assigned/zones"
+          component={ViewAssignedZones}
+        />
+        <PrivateRoute
+          exact
+          path="/view/assigned/clusters"
+          component={ViewAssignedClusters}
+        />
+      </Switch>
+    </Router>
+  )
 }
+export default ZoneClusterRouter
