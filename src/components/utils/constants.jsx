d05fa2ebcc4132c5ac9b98e554a685ab53708208
Anjana Nagaraja Mucharla <mn.anjana@publicissapient.com>
2020-04-14 16:03:51 +0530

    commited

---
+++ src/components/utils/constants.jsx
@@ -0,0 +1,2 @@
+export const  withdrawClusterPromotion = ["Promotion Percentage", "Actual Price", "Promotion From Date", "Promotion To Date", "Withdraw"];
+export const viewZones = ["Zone Name", "Number of Zones"];
