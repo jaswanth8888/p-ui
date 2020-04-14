d2fc39316d20be7e0e0773788789a84925ca8971
mayrao1 <mayrao1@publicisgroupe.net>
2020-04-14 15:30:22 +0530

    Removed inline styling

--- src/components/vendor/Registration.jsx
+++ src/components/vendor/Registration.jsx
@@ -72,6 +72,7 @@ export class Registration extends Component {
   isValidEmail = () => {
     const { email } = this.state.vendorDetails
     const { error } = this.state
+    // eslint-disable-next-line no-useless-escape
     const re = /^(([^<>()[\]\\.,:\s@\"]+(\.[^<>()[\]\\.,:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!re.test(email)) {
       error.emailError = true
