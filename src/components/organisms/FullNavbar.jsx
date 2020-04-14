25f9271617e270e8d4eb92aa9dc37f6bb25573b1
mayrao1 <mayrao1@publicisgroupe.net>
2020-04-14 12:58:23 +0530

    Removed inline styling

--- src/components/organisms/FullNavbar.jsx
+++ src/components/organisms/FullNavbar.jsx
@@ -410,7 +410,7 @@ function FullNavbar(props) {
                     sessionStorage.setItem("countryCode", countryCode)
                   }}
                 />
-              </>
+              </null>
             )}
           </Toolbar>
         </AppBar>
