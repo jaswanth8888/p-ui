6dd8234a6feeb685cc9da7ec2e1ec8c02b444a00
mayrao1 <mayrao1@publicisgroupe.net>
2020-04-19 15:34:25 +0530

    Fixed CSS breaking and effectiveprice

--- src/components/retailer/AddProducts.jsx
+++ src/components/retailer/AddProducts.jsx
@@ -97,205 +97,207 @@ class AddProducts extends Component {
     const { categories, products } = this.props
     const { category, numberBoxInputValue, quantityCheck } = this.state
     return (
-      <div className="box-container add-prods">
-        <Typography className="card-header" variant="h4">
-          {addProductToStore}
-        </Typography>
-        <div className="product-form-header">
-          <FormControl variant="outlined" fullWidth>
-            <InputLabel htmlFor="category">Enter Category</InputLabel>
-            <Select
-              fullWidth
-              native
-              variant="outlined"
-              label="Enter Category"
-              value={category}
-              onChange={this.handleChangeCategory}
-              inputProps={{
-                name: "category",
-                id: "category",
-              }}
-            >
-              <option aria-label="None" value="" />
-              {categories.map((categoryVal) => {
-                return <option value={categoryVal}>{categoryVal}</option>
-              })}
-            </Select>
-          </FormControl>
-        </div>
-        <div className="product-form-body">
-          <form className="productform">
-            <TableContainer component={Paper}>
-              <Table size="small" aria-label="a dense table">
-                <TableHead>
-                  <TableRow>
-                    {/* <TableCell>CheckBox</TableCell> */}
-                    {/* <TableCell>Select</TableCell>
+      <div className="box-container">
+        <div className="joint-products-form">
+          <Typography className="card-header" variant="h4">
+            {addProductToStore}
+          </Typography>
+          <div className="product-form-header">
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel htmlFor="category">Enter Category</InputLabel>
+              <Select
+                fullWidth
+                native
+                variant="outlined"
+                label="Enter Category"
+                value={category}
+                onChange={this.handleChangeCategory}
+                inputProps={{
+                  name: "category",
+                  id: "category",
+                }}
+              >
+                <option aria-label="None" value="" />
+                {categories.map((categoryVal) => {
+                  return <option value={categoryVal}>{categoryVal}</option>
+                })}
+              </Select>
+            </FormControl>
+          </div>
+          <div className="product-form-body">
+            <form className="productform">
+              <TableContainer component={Paper}>
+                <Table size="small" aria-label="a dense table">
+                  <TableHead>
+                    <TableRow>
+                      {/* <TableCell>CheckBox</TableCell> */}
+                      {/* <TableCell>Select</TableCell>
                     <TableCell>Product Name</TableCell>
                     <TableCell>Price</TableCell>
                     <TableCell>Vendor Name</TableCell>
                     <TableCell>Quantity Available</TableCell>
                     <TableCell>Quantity Required</TableCell> */}
-                    {addProduct.map((tcell) => (
-                      <TableCell>{tcell}</TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
+                      {addProduct.map((tcell) => (
+                        <TableCell>{tcell}</TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
 
-                <tbody>
-                  {products.map((product) => {
-                    return (
-                      <TableRow key={product.id}>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <FormControlLabel
-                              // control={<input type="checkbox" id={product.id} color="primary" value={JSON.stringify(product)}
-                              // />}
-                              control={
-                                <Checkbox
-                                  id={product.id}
-                                  name="checkedB"
-                                  color="primary"
-                                  value={JSON.stringify(product)}
-                                />
-                              }
-                            />
-                          ) : (
-                            <FormControlLabel
-                              // control={<input type="checkbox" id={product.id} color="primary" value={JSON.stringify(product)}
-                              // />}
-                              control={
-                                <Checkbox
-                                  id={product.id}
-                                  name="checkedB"
-                                  color="primary"
-                                  value={JSON.stringify(product)}
-                                  disabled
-                                />
-                              }
-                            />
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <Typography variant="subtitle1" gutterBottom>
-                              {product.productName}
-                            </Typography>
-                          ) : (
-                            <Typography
-                              variant="subtitle1"
-                              className="disabled"
-                              gutterBottom
-                            >
-                              {product.productName}
-                            </Typography>
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <Typography variant="subtitle1" gutterBottom>
-                              {product.productBasePrice}
-                            </Typography>
-                          ) : (
-                            <Typography
-                              variant="subtitle1"
-                              className="disabled"
-                              gutterBottom
-                            >
-                              {product.productBasePrice}
-                            </Typography>
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <Typography variant="subtitle1" gutterBottom>
-                              {product.companyName}
-                            </Typography>
-                          ) : (
-                            <Typography
-                              variant="subtitle1"
-                              className="disabled"
-                              gutterBottom
-                            >
-                              {product.companyName}
-                            </Typography>
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <Typography variant="subtitle1" gutterBottom>
-                              {product.remainingQuantity}
-                            </Typography>
-                          ) : (
-                            <Typography
-                              variant="subtitle1"
-                              className="disabled"
-                              gutterBottom
-                            >
-                              {product.remainingQuantity}
-                            </Typography>
-                          )}
-                        </TableCell>
-                        <TableCell>
-                          {product.remainingQuantity > 0 ? (
-                            <TextField
-                              fullWidth
-                              type="number"
-                              id={product.id}
-                              value={numberBoxInputValue[product.id]}
-                              pattern="[0-9]*"
-                              min="0"
-                              max={product.remainingQuantity}
-                              step="1"
-                              onKeyDown={(evt) =>
-                                evt.key === "." && evt.preventDefault()
-                              }
-                              onChange={this.updateInputValue}
-                              label="Quantity"
-                              variant="outlined"
-                            />
-                          ) : (
-                            <TextField
-                              fullWidth
-                              type="number"
-                              id={product.id}
-                              value={numberBoxInputValue[product.id]}
-                              pattern="[0-9]*"
-                              min="0"
-                              max={product.remainingQuantity}
-                              step="1"
-                              onKeyDown={(evt) =>
-                                evt.key === "e" && evt.preventDefault()
-                              }
-                              onChange={this.updateInputValue}
-                              label="Quantity"
-                              variant="outlined"
-                              disabled
-                            />
-                          )}
-                        </TableCell>
-                      </TableRow>
-                    )
-                  })}
-                </tbody>
-              </Table>
-            </TableContainer>
-            {quantityCheck && (
-              <Link to="/addproducts" className="special-href" id="top-pad">
-                <Button
-                  type="button"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  className="{classes.submit}"
-                  onClick={this.loopForm}
-                  id="add-product-submit"
-                >
-                  ADD PRODUCTS
-                </Button>
-              </Link>
-            )}
-          </form>
+                  <tbody>
+                    {products.map((product) => {
+                      return (
+                        <TableRow key={product.id}>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <FormControlLabel
+                                // control={<input type="checkbox" id={product.id} color="primary" value={JSON.stringify(product)}
+                                // />}
+                                control={
+                                  <Checkbox
+                                    id={product.id}
+                                    name="checkedB"
+                                    color="primary"
+                                    value={JSON.stringify(product)}
+                                  />
+                                }
+                              />
+                            ) : (
+                              <FormControlLabel
+                                // control={<input type="checkbox" id={product.id} color="primary" value={JSON.stringify(product)}
+                                // />}
+                                control={
+                                  <Checkbox
+                                    id={product.id}
+                                    name="checkedB"
+                                    color="primary"
+                                    value={JSON.stringify(product)}
+                                    disabled
+                                  />
+                                }
+                              />
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <Typography variant="subtitle1" gutterBottom>
+                                {product.productName}
+                              </Typography>
+                            ) : (
+                              <Typography
+                                variant="subtitle1"
+                                className="disabled"
+                                gutterBottom
+                              >
+                                {product.productName}
+                              </Typography>
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <Typography variant="subtitle1" gutterBottom>
+                                {product.productBasePrice}
+                              </Typography>
+                            ) : (
+                              <Typography
+                                variant="subtitle1"
+                                className="disabled"
+                                gutterBottom
+                              >
+                                {product.productBasePrice}
+                              </Typography>
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <Typography variant="subtitle1" gutterBottom>
+                                {product.companyName}
+                              </Typography>
+                            ) : (
+                              <Typography
+                                variant="subtitle1"
+                                className="disabled"
+                                gutterBottom
+                              >
+                                {product.companyName}
+                              </Typography>
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <Typography variant="subtitle1" gutterBottom>
+                                {product.remainingQuantity}
+                              </Typography>
+                            ) : (
+                              <Typography
+                                variant="subtitle1"
+                                className="disabled"
+                                gutterBottom
+                              >
+                                {product.remainingQuantity}
+                              </Typography>
+                            )}
+                          </TableCell>
+                          <TableCell>
+                            {product.remainingQuantity > 0 ? (
+                              <TextField
+                                fullWidth
+                                type="number"
+                                id={product.id}
+                                value={numberBoxInputValue[product.id]}
+                                pattern="[0-9]*"
+                                min="0"
+                                max={product.remainingQuantity}
+                                step="1"
+                                onKeyDown={(evt) =>
+                                  evt.key === "." && evt.preventDefault()
+                                }
+                                onChange={this.updateInputValue}
+                                label="Quantity"
+                                variant="outlined"
+                              />
+                            ) : (
+                              <TextField
+                                fullWidth
+                                type="number"
+                                id={product.id}
+                                value={numberBoxInputValue[product.id]}
+                                pattern="[0-9]*"
+                                min="0"
+                                max={product.remainingQuantity}
+                                step="1"
+                                onKeyDown={(evt) =>
+                                  evt.key === "e" && evt.preventDefault()
+                                }
+                                onChange={this.updateInputValue}
+                                label="Quantity"
+                                variant="outlined"
+                                disabled
+                              />
+                            )}
+                          </TableCell>
+                        </TableRow>
+                      )
+                    })}
+                  </tbody>
+                </Table>
+              </TableContainer>
+              {quantityCheck && (
+                <Link to="/addproducts" className="special-href" id="top-pad">
+                  <Button
+                    type="button"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className="{classes.submit}"
+                    onClick={this.loopForm}
+                    id="add-product-submit"
+                  >
+                    ADD PRODUCTS
+                  </Button>
+                </Link>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     )
