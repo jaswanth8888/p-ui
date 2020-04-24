aca13a9f3b34c8433967fbbd1d11deffd027a601
KARANI JASWANTH <karani.jaswanth@publicissapient.com>
2020-04-24 11:06:25 +0530

    added id for dashboard button

--- src/components/organisms/FullNavbar.jsx
+++ src/components/organisms/FullNavbar.jsx
@@ -124,7 +124,7 @@ function FullNavbar(props) {
       <List>
         <Link to="/dashboard">
           <Tooltip title="Dashboard" placement="right">
-            <ListItem button>
+            <ListItem button id="dashboard-btn">
               <ListItemIcon>
                 <DateRangeIcon />
               </ListItemIcon>
