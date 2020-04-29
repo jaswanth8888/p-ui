dc0b4dc1ac55b3de0b6e52a434ec44705fa2f214
Pawan Kumar Naveen <pawan.naveen@publicissapient.com>
2020-04-29 10:45:44 +0530

    sprint7

--- src/components/retailer/DefinePromotionInZone.jsx
+++ src/components/retailer/DefinePromotionInZone.jsx
@@ -28,6 +28,7 @@ class DefinePromotionInZone extends Component {
         endDate: "",
         promotionPercentage: "",
         zoneName: zone,
+        addedBy: "",
       },
       levelOption: "zone",
     };
