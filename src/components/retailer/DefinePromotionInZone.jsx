6b9b7de33f8d448036e486d440662f4b27eb1805
Pawan Kumar Naveen <pawan.naveen@publicissapient.com>
2020-04-29 11:02:20 +0530

    sprint7

--- src/components/retailer/DefinePromotionInZone.jsx
+++ src/components/retailer/DefinePromotionInZone.jsx
@@ -167,23 +167,23 @@ class DefinePromotionInZone extends Component {
               <div className="pt-10">
                 <div className="pt-10">
                   {loggedInUser.userType === "admin" &&
-                  promotionAlert.promotionHasBeenAppliedLast72hours === 1 ? (
-                    <Alert severity="info">
-                      "A promotion was defined in the last 72 hours. Further
-                      addition requires retailer approval"
-                    </Alert>
-                  ) : (
-                    ""
-                  )}
+                    promotionAlert.promotionHasBeenAppliedLast72hours === 1 ? (
+                      <Alert severity="info">
+                        A promotion was defined in the last 72 hours. Further
+                        addition requires retailer approval
+                      </Alert>
+                    ) : (
+                      ""
+                    )}
                 </div>
 
                 <div className="pt-10">
                   {loggedInUser.userType === "admin" &&
-                  promotionAlert.promotionAlreadyApplied === 1 ? (
-                    <Alert severity="info"> "Promotion already defined"</Alert>
-                  ) : (
-                    ""
-                  )}
+                    promotionAlert.promotionAlreadyApplied === 1 ? (
+                      <Alert severity="info"> Promotion already defined</Alert>
+                    ) : (
+                      ""
+                    )}
                 </div>
               </div>
               <Typography className="card-header" variant="h6">
@@ -271,6 +271,8 @@ DefinePromotionInZone.propTypes = {
   postPromotion: PropTypes.func.isRequired,
   history: PropTypes.shape.isRequired,
   assignedPrice: PropTypes.string.isRequired,
+  loggedInUser: PropTypes.shape.isRequired,
+  promotionAlert: PropTypes.shape.isRequired,
 }
 const stateAsProps = (store) => ({
   productDetails: store.RetailerReducer.productDetails,
