0d2338c6530b028fe7998e8741041eed33cad413
Godavarthi Karteek <godkarte@publicisgroupe.net>
2020-05-03 22:31:27 +0530

    Fixed i18n bug in addProduct

--- src/components/organisms/FullNavbar.jsx
+++ src/components/organisms/FullNavbar.jsx
@@ -160,7 +160,7 @@ function FullNavbar(props) {
                 </ListItemIcon>
                 <ListItemText
                   className="list-item-text"
-                  primary="Approve Promotions"
+                  primary={t("welcome.approvePromotions")}
                 />
               </ListItem>
             </Tooltip>
